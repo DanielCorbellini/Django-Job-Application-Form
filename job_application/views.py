dea69c1dbb07e33957c3a891ca886a7ded1f8917
Daniel <daniel.corbellini28@gmail.com>
2023-08-28 15:29:48 -0300

    Store the data in the database and creating the message when the user press the submit button

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from job_application.forms import ApplicationForm
+from job_application.models import Form
 
 
 def index(request):
@@ -12,4 +13,10 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date,
+                                occupation=occupation)
+            messages.success(request, "Form submitted successfully!")
+
+            return redirect("index")
     return render(request, "index.html")
